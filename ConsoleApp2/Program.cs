8f41e96e1184673b8efec229038cd8c30fd76d17
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:50:51 +0200

    Converte un numero in binario/ottale/esadecimale

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,15 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("inserisci un numero da convertire");
+            int valute = int.Parse(Console.ReadLine());
+            string binary = Convert.ToString(valute, 2);
+            Console.WriteLine($"Il numero binario è {binary}");
+            string ottale = Convert.ToString(valute, 8);
+            Console.WriteLine($"Il numero ottale è {ottale}");
+            string esadecimale = Convert.ToString(valute, 16);
+            Console.WriteLine($"Il numero esadecimale è {esadecimale}");
+            Console.ReadLine();
         }
     }
 }
